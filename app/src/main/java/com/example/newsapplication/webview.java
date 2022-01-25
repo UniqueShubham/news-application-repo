56afd176eae80e9d9d8dcd08505cf33630d10289
UniqueShubham <kevadiya.shubham@gmail.com>
2022-01-25 15:56:58 +0530

    News Card Adapter and UI

---
+++ app/src/main/java/com/example/newsapplication/webview.java
@@ -0,0 +1,28 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toolbar;
+
+public class webview extends AppCompatActivity {
+
+    Toolbar toolbar;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        toolbar = findViewById(R.id.toolBar);
+        webView = findViewById(R.id.wvNews);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
